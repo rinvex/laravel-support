b5ec3f565d5cb3d5434cc7ec87a7e4deee148cff
Abdelrahman Omran <me@omranic.com>
2016-12-17 18:34:36 +0200

    Remove rinvex/repository related stuff

--- src/Providers/BaseServiceProvider.php
+++ src/Providers/BaseServiceProvider.php
@@ -20,13 +20,6 @@ use Illuminate\Support\ServiceProvider;
 abstract class BaseServiceProvider extends ServiceProvider
 {
     /**
-     * The alias pattern.
-     *
-     * @var string
-     */
-    protected $aliasPattern = '{{class}}Contract';
-
-    /**
      * Register an IoC binding if it's not already been registered.
      *
      * @param string               $abstract
@@ -36,49 +29,11 @@ abstract class BaseServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    protected function bind($abstract, $concrete = null, $shared = true, $force = false)
+    protected function bindIfNotBound($abstract, $concrete = null, $shared = true, $force = false)
     {
         if (! $this->app->bound($abstract) || $force) {
             $concrete = $concrete ?: $abstract;
             $this->app->bind($abstract, $concrete, $shared);
         }
-    }
-
-    /**
-     * Register an IoC binding whether it's already been registered or not.
-     *
-     * @param string               $abstract
-     * @param \Closure|string|null $concrete
-     * @param bool                 $shared
-     * @param string|null          $alias
-     * @param bool                 $force
-     *
-     * @return void
-     */
-    protected function bindAndAlias($abstract, $concrete = null, $shared = true, $alias = null, $force = false)
-    {
-        if (! $this->app->bound($abstract) || $force) {
-            $concrete = $concrete ?: $abstract;
-            $this->app->bind($abstract, $concrete, $shared);
-            $this->app->alias($abstract, $this->prepareAlias($alias, $concrete));
-        }
-    }
-
-    /**
-     * Prepare the alias.
-     *
-     * @param string|null $alias
-     * @param mixed       $concrete
-     *
-     * @return string
-     */
-    protected function prepareAlias($alias, $concrete)
-    {
-        if (! $alias && ! $concrete instanceof \Closure) {
-            $concrete = str_replace('Repositories', 'Contracts', $concrete);
-            $alias    = str_replace('{{class}}', $concrete, $this->aliasPattern);
-        }
-
-        return $alias;
     }
 }
