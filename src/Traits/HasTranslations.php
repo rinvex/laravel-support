002f45b8e2c4164a11ac00a27ddf26dcf6719396
Abdelrahman Omran <me@omranic.com>
2017-08-30 05:19:39 +0200

    Handle default translation if no locale supplied

--- src/Traits/HasTranslations.php
+++ src/Traits/HasTranslations.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Rinvex\Support\Traits;
 
+use Illuminate\Support\Str;
+use Spatie\Translatable\Events\TranslationHasBeenSet;
 use Spatie\Translatable\HasTranslations as BaseHasTranslations;
 
 trait HasTranslations
@@ -22,5 +24,35 @@ trait HasTranslations
         }
 
         return $this->getTranslation($key, config('app.locale')) ?: array_first($this->getTranslations($key));
+    }
+
+    /**
+     * Get translations.
+     *
+     * @param $key
+     *
+     * @return array
+     */
+    public function getTranslations($key): array
+    {
+        $this->guardAgainstUntranslatableAttribute($key);
+        $value = json_decode($this->getAttributes()[$key] ?? '' ?: '{}', true);
+
+        // Inject default translation if none supplied
+        if (! is_array($value)) {
+            $oldValue = $value;
+
+            if ($this->hasSetMutator($key)) {
+                $method = 'set'.Str::studly($key).'Attribute';
+                $value = $this->{$method}($value);
+            }
+
+            $value = [$locale = app()->getLocale() => $value];
+
+            $this->attributes[$key] = $this->asJson($value);
+            event(new TranslationHasBeenSet($this, $key, $locale, $oldValue, $value));
+        }
+
+        return $value;
     }
 }
