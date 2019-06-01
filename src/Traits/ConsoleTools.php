b3da70e5e05cd6bc956d26903b850e151fb990ab
Abdelrahman Omran <me@omranic.com>
2019-06-01 11:35:02 +0200

    Add bunch of helpful console tools to be used in Service Providers

---
+++ src/Traits/ConsoleTools.php
@@ -0,0 +1,103 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Rinvex\Support\Traits;
+
+trait ConsoleTools
+{
+    /**
+     * Publish package migrations.
+     *
+     * @return void
+     */
+    protected function publishMigrations(string $package): void
+    {
+        $namespace = str_replace('laravel-', '', $package);
+        $namespace = str_replace(['/', '\\', '.', '_'], '-', $namespace);
+        $basePath = starts_with($package, 'cortex') ? $this->app->path($package)
+            : $this->app->basePath('vendor/'.$package);
+
+        if (file_exists($path = $basePath.'/database/migrations')) {
+            $stubs = $this->app['files']->glob($path.'/*.php.stub');
+            $existing = $this->app['files']->glob($this->app->databasePath('migrations/'.$package.'/*.php'));
+
+            $migrations = collect($stubs)->flatMap(function ($migration) use ($existing, $package) {
+                $sequence = substr(basename($migration), 0, 2);
+                $match = collect($existing)->first(function ($item, $key) use ($migration, $sequence) {
+                    return strpos($item, str_replace(['.stub', $sequence], '', basename($migration))) !== false;
+                });
+
+                return [$migration => $this->app->databasePath('migrations/'.$package.'/'.($match ? basename($match) : date('Y_m_d_His', time() + $sequence).str_replace(['.stub', $sequence], '', basename($migration))))];
+            })->toArray();
+
+            $this->publishes($migrations, $namespace.'-migrations');
+        }
+    }
+
+    /**
+     * Publish package config.
+     *
+     * @return void
+     */
+    protected function publishConfig(string $package): void
+    {
+        $namespace = str_replace('laravel-', '', $package);
+        $namespace = str_replace(['/', '\\', '.', '_'], '-', $namespace);
+        $basePath = starts_with($package, 'cortex') ? $this->app->path($package)
+            : $this->app->basePath('vendor/'.$package);
+
+        if (file_exists($path = $basePath.'/config/config.php')) {
+            $this->publishes([$path => $this->app->configPath(str_replace('-', '.', $namespace).'.php')], $namespace.'-config');
+        }
+    }
+
+    /**
+     * Publish package views.
+     *
+     * @return void
+     */
+    protected function publishViews(string $package): void
+    {
+        $namespace = str_replace('laravel-', '', $package);
+        $namespace = str_replace(['/', '\\', '.', '_'], '-', $namespace);
+        $basePath = starts_with($package, 'cortex') ? $this->app->path($package)
+            : $this->app->basePath('vendor/'.$package);
+
+        if (file_exists($path = $basePath.'/resources/views')) {
+            $this->publishes([$path => $this->app->resourcePath('views/vendor/'.$package)], $namespace.'-views');
+        }
+    }
+
+    /**
+     * Publish package lang.
+     *
+     * @return void
+     */
+    protected function publishLang(string $package): void
+    {
+        $namespace = str_replace('laravel-', '', $package);
+        $namespace = str_replace(['/', '\\', '.', '_'], '-', $namespace);
+        $basePath = starts_with($package, 'cortex') ? $this->app->path($package)
+            : $this->app->basePath('vendor/'.$package);
+
+        if (file_exists($path = $basePath.'/resources/lang')) {
+            $this->publishes([$path => $this->app->resourcePath('lang/vendor/'.$package)], $namespace.'-lang');
+        }
+    }
+
+    /**
+     * Register console commands.
+     *
+     * @return void
+     */
+    protected function registerCommands(): void
+    {
+        // Register artisan commands
+        foreach ($this->commands as $key => $value) {
+            $this->app->singleton($value, $key);
+        }
+
+        $this->commands(array_values($this->commands));
+    }
+}
